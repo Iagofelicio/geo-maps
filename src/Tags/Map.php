f7abfda2ffac54eaf4706bb0dd20417cd6ddf476
Eric Celeste <efc@clst.org>
2025-11-11 10:42:18 -0600

    escape quotes from text

--- src/Tags/Map.php
+++ src/Tags/Map.php
@@ -210,7 +210,7 @@ class Map extends Tags
                 $markerTmpString = explode('|',$value);
                 foreach($markerTmpString as $option){
                     $optionKey = (explode('=',$option))[0];
-                    $optionValue = (explode('=',$option,2))[1];
+                    $optionValue = str_replace("'","\\'",(explode('=',$option,2))[1]);
                     match ($optionKey) {
                         'lat' => $lat = $optionValue,
                         'lon' => $lon = $optionValue,
